f7c859a194dd754134759ba6a80ab5e8471c5a73
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2024-09-23 21:56:47 -0300

    Refact

--- internal/network/client.go
+++ internal/network/client.go
@@ -11,9 +11,11 @@ import (
 )
 
 const (
-	BaseURL     = "game.go-go.dev"
-	writeWait   = 10 * time.Second
-	readTimeout = 60 * time.Second
+	// BaseURL is the base URL of the server.
+	BaseURL = "game.go-go.dev"
+
+	writeTimeout = 10 * time.Second
+	readTimeout  = 60 * time.Second
 )
 
 type Client struct {
@@ -34,7 +36,7 @@ func NewClient(ctx context.Context, cancel context.CancelFunc, serverURL string)
 func (c *Client) Connect() error {
 	u := fmt.Sprintf("wss://%s/multiplayer", c.serverURL)
 
-	ctx, cancel := context.WithTimeout(c.ctx, writeWait)
+	ctx, cancel := context.WithTimeout(c.ctx, writeTimeout)
 	defer cancel()
 
 	conn, _, err := websocket.Dial(ctx, u, nil)
@@ -43,25 +45,24 @@ func (c *Client) Connect() error {
 	}
 
 	c.conn = conn
-	slog.Info("WebSocket connection established", slog.String("url", u))
+
+	slog.Info("websocket connection established", slog.String("url", u))
+
 	return nil
 }
 
-func (c *Client) ReceiveGameState(gameStateChan chan<- GameState) error {
+// ReceiveGameState receives the game state from the server and sends it to the given channel.
+func (c *Client) ReceiveGameState(gameStateChan chan<- GameState) {
 	defer close(gameStateChan)
 
 	for {
 		select {
 		case <-c.ctx.Done():
-			slog.Info("Client context canceled, closing message handler")
-			return nil
+			slog.Info("client context canceled, closing message handler")
 		default:
-			ctx, cancel := context.WithTimeout(c.ctx, readTimeout)
-			defer cancel()
-
 			var gameState GameState
-			if err := wsjson.Read(ctx, c.conn, &gameState); err != nil {
-				return fmt.Errorf("failed to read game state: %w", err)
+			if err := wsjson.Read(c.ctx, c.conn, &gameState); err != nil {
+				slog.Error("failed to read game state: %w", slog.Any("error", err))
 			}
 
 			gameStateChan <- gameState
@@ -71,37 +72,43 @@ func (c *Client) ReceiveGameState(gameStateChan chan<- GameState) error {
 
 func (c *Client) Close() {
 	c.cancel()
+
 	if c.conn == nil {
 		return
 	}
 
 	if err := c.conn.Close(websocket.StatusNormalClosure, "normal closure"); err != nil {
-		slog.Error("Error closing WebSocket connection", slog.Any("error", err))
+		slog.Error("error closing WebSocket connection", slog.Any("error", err))
 	} else {
-		slog.Info("WebSocket connection closed gracefully")
+		slog.Info("websocket connection closed gracefully")
 	}
 }
 
+// SendPlayerInfo sends the player info to the server.
+// It's used to register the player in the server.
 func (c *Client) SendPlayerInfo(gi GameInfo) error {
-	ctx, cancel := context.WithTimeout(c.ctx, writeWait)
+	ctx, cancel := context.WithTimeout(c.ctx, writeTimeout)
 	defer cancel()
 
 	if err := wsjson.Write(ctx, c.conn, gi); err != nil {
 		return fmt.Errorf("failed to send player info: %w", err)
 	}
 
-	slog.Info("Player info sent successfully")
+	slog.Info("player info sent successfully")
+
 	return nil
 }
 
+// SendPlayerInput sends the player input to the server.
 func (c *Client) SendPlayerInput(input PlayerInput) error {
-	ctx, cancel := context.WithTimeout(c.ctx, writeWait)
+	ctx, cancel := context.WithTimeout(c.ctx, writeTimeout)
 	defer cancel()
 
 	if err := wsjson.Write(ctx, c.conn, input); err != nil {
 		return fmt.Errorf("failed to send player input: %w", err)
 	}
 
-	slog.Info("Player input sent successfully")
+	slog.Info("player input sent successfully")
+
 	return nil
 }
