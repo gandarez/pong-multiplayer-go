bf35622a23ac370a5f8f5db99d9a22854f949169
reneepc <rcardozo@tractian.com>
2024-09-23 21:23:32 -0300

    Add draw name function

--- internal/game/player.go
+++ internal/game/player.go
@@ -2,10 +2,14 @@ package game
 
 import (
 	"image/color"
+	"log/slog"
 
 	"github.com/hajimehoshi/ebiten/v2"
 
+	"github.com/gandarez/pong-multiplayer-go/internal/font"
+	"github.com/gandarez/pong-multiplayer-go/internal/ui"
 	engineplayer "github.com/gandarez/pong-multiplayer-go/pkg/engine/player"
+	"github.com/gandarez/pong-multiplayer-go/pkg/geometry"
 )
 
 // drawPlayer draws a player on the screen.
@@ -15,4 +19,19 @@ func drawPlayer(p engineplayer.Player, screen *ebiten.Image) {
 			screen.Set(int(p.Position().X+x), int(p.Position().Y+y), color.RGBA{200, 200, 200, 255})
 		}
 	}
+}
+
+func drawName(name string, namePosition geometry.Vector, screen *ebiten.Image, font *font.Font) {
+	textface, err := font.Face("ui", 12)
+	if err != nil {
+		slog.Error("failed to get text face to draw player name", slog.Any("error", err))
+		return
+	}
+	t := ui.Text{
+		Value:    name,
+		FontFace: textface,
+		Position: namePosition,
+		Color:    color.RGBA{200, 200, 200, 200},
+	}
+	t.Draw(screen)
 }
